9fe6123ece4d103d494fd6fb3e47784e0e5f3dd1
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-04-04 13:28:35 -0700

    Researched and changed consumers to allow for kicking

--- OBChat/OB/commands.py
+++ OBChat/OB/commands.py
@@ -1,7 +1,7 @@
 from asgiref.sync import async_to_sync
 from channels.layers import get_channel_layer
 from . import constants, models
-from .consumers import ChatConsumer, GroupTypes, group_name
+from .consumers import ChatConsumer, GroupTypes, get_group_name, SystemOperations
 
 def command(data, user, room_name):
     # Separate by whitespace to get arguments
@@ -13,15 +13,24 @@ def command(data, user, room_name):
         constants.COMMANDS[command_name](args, user, room_name)
     except KeyError:
         # Invalid command, send the list of valid commands
-        send(constants.VALID_COMMANDS, room_name)
+        send_chat_message(constants.VALID_COMMANDS, room_name)
 
-def send(message, group):
-    # Send message to room group
+def send_chat_message(message, group):
     async_to_sync(get_channel_layer().group_send)(
         group,
         {
-            "type": "receive_from_group",
+            "type": "chat_message",
             "message": message
+        }
+    )
+
+def send_system_operation(operation, user, group):
+    async_to_sync(get_channel_layer().group_send)(
+        group,
+        {
+            "type": "system_operation",
+            "operation": operation,
+            "user": user
         }
     )
 
@@ -38,7 +47,7 @@ def send(message, group):
 #         False if an error occurred
 #     """
 
-#     send("Available rooms:", client)
+#     send_chat_message("Available rooms:", client)
 
 #     for room in self.rooms:
 #         room_str = f" * {room} ({len(self.rooms[room].users)})"
@@ -59,9 +68,9 @@ def send(message, group):
 #         if client.username in room.banned:
 #             room_str += " (banned)"
 
-#         send(room_str, client)
+#         send_chat_message(room_str, client)
 
-#     send("End list", client)
+#     send_chat_message("End list", client)
 
 #     return True
 
@@ -76,20 +85,19 @@ def who(args, user, room_name):
     if not models.Room.objects.filter(name=args[0]).exists():
         error_message = f"\"{args[0]}\" doesn't exist, so that probably means nobody is in there."
     else:
-        channel = get_channel_layer()
-        group = Group(channel).channel_layer.group_channels(channel)
-    
-        if len(group) == 0:
+        # TODO: Get the room's list of current users
+
+        if False: # TODO: len(current_users) == 0:
             error_message = f"\"{args[0]}\" is all empty!"
 
-    group = group_name(GroupTypes.Room, room_name)
+    group_name = get_group_name(GroupTypes.Room, room_name)
 
     if error_message:
-        send(error_message, group)
+        send_chat_message(error_message, group_name)
         return
 
     # Iterate through users in room
-    user_list = f"Users in: \"{args[0]}\" ({group})\n"
+    user_list = f"Users in: \"{args[0]}\" ({group_name})\n"
 
     current_room = models.Room.objects.get(name=room_name)
 
@@ -108,12 +116,12 @@ def who(args, user, room_name):
 
         user_list += f"\n * {who_user}\nEnd list"
 
-    send(user_list, group)
+    send_chat_message(user_list, group)
 
 # def leave(self, args, client, exit=False):
 #     # Client is not in a room
 #     if client.room is None:
-#         send("Not in a room", client)
+#         send_chat_message("Not in a room", client)
 
 #         return False
 
@@ -130,7 +138,7 @@ def who(args, user, room_name):
 
 #     # Don't print the leave message when exiting
 #     if not exit:
-#         send(f"Left the room: {client.room.name}", client)
+#         send_chat_message(f"Left the room: {client.room.name}", client)
 
 #     client.room.users.remove(client)
 #     client.room = None
@@ -153,7 +161,7 @@ def private(args, user, room_name):
         error_message = "No message specified. Did you give up halfway through?"
 
     if error_message:
-        send(error_message, room_name)
+        send_chat_message(error_message, room_name)
         return
 
     send_to = user_query.first()
@@ -161,7 +169,7 @@ def private(args, user, room_name):
     # Reconstruct message from args
     message = " ".join(args[1:])
 
-    send(message, group_name(GroupTypes.Private, user.username, send_to.username))
+    send_chat_message(message, get_group_name(GroupTypes.Private, user.username, send_to.username))
 
 def room(args, user, room_name):
     if not args:
@@ -176,7 +184,7 @@ def room(args, user, room_name):
         models.Room(name=room_name, owner=user).save()
         return_message = f"Sold! Check out your new room: \"{args[0]}\""
 
-    send (return_message, group_name(GroupTypes.Room, room_name))
+    send_chat_message(return_message, get_group_name(GroupTypes.Room, room_name))
 
 def hire(args, user, room_name):
     current_room = models.Room.objects.get(room_name)
@@ -220,16 +228,16 @@ def hire(args, user, room_name):
     for hired_user in valid_hires:
         models.Admin(user=hired_user, room=current_room).save()
 
-        send(f"With great power comes great responsibility. You were promoted to admin in \"\
-            {room_name}\"!", group_name(GroupTypes.Line, hired_user.username))
+        send_chat_message(f"With great power comes great responsibility. You were promoted to admin in \"\
+            {room_name}\"!", get_group_name(GroupTypes.Line, hired_user.username))
         
-        send_to_sender += f"Promoted {hired_user.username} to admin. Keep an eye on them."
+        send_to_sender += f"Promoted {hired_user.username} to admin in {room_name}. Keep an eye on them."
         send_to_others += f"{hired_user.username} was promoted to admin. Drinks on them!"
 
     if send_to_sender:
-        send("\n".join(send_to_sender), group_name(GroupTypes.Line, user.username))
+        send_chat_message("\n".join(send_to_sender), get_group_name(GroupTypes.Line, user.username))
     if send_to_others:
-        send("\n".join(send_to_others), group_name(GroupTypes.Room, room_name))
+        send_chat_message("\n".join(send_to_others), get_group_name(GroupTypes.Room, room_name))
 
 def fire(args, user, room_name):
     current_room = models.Room.objects.get(room_name)
@@ -267,23 +275,23 @@ def fire(args, user, room_name):
             else:
                 valid_fires += (user_query, admin_query)
             
-    # Remove admin(s) and notify all parties that a user was fired 
+    # Remove admin(s) and notify all parties that a user was fired
     send_to_sender = error_messages
     send_to_others = []
     
     for fired_user in valid_fires:
         fired_user[1].delete()
 
-        send(f"Clean out your desk. You lost your adminship at \"{room_name}\".",  
-            group_name(GroupTypes.Line, fired_user[0].username)) 
+        send_chat_message(f"Clean out your desk. You lost your adminship at \"{room_name}\".",  
+            get_group_name(GroupTypes.Line, fired_user[0].username)) 
 
         send_to_sender += f"It had to be done. You fired \"{fired_user[0].username}\""
         send_to_others += f"{fired_user[0].username} was fired! Those budget cuts are killer."
 
     if send_to_sender:
-        send("\n".join(send_to_sender), group_name(GroupTypes.Line, user.username))
+        send_chat_message("\n".join(send_to_sender), get_group_name(GroupTypes.Line, user.username))
     if send_to_others:
-        send("\n".join(send_to_others), group_name(GroupTypes.Room, room_name))
+        send_chat_message("\n".join(send_to_others), get_group_name(GroupTypes.Room, room_name))
 
 def kick(args, user, room_name):
     current_room = models.Room.objects.get(room_name)
@@ -317,14 +325,25 @@ def kick(args, user, room_name):
                     love some more paperwork to do..."
             else:
                 valid_kicks += user_query
-    
+
     # Remove user(s) and notify all parties that a user was kicked
     send_to_sender = error_messages
     send_to_others = []
 
-    for kicked_user in valid_kicks:
-        # TODO: finish this
+    room_group = get_group_name(GroupTypes.Room, room_name)
 
+    for kicked_user in valid_kicks:
+        send_system_operation(SystemOperations.Kick, kicked_user, room_group)
+
+        send_chat_message(f"You were kicked from {room_name}. Check yourself before you wreck yourself.", get_group_name(GroupTypes.Line, kicked_user.username))
+
+        send_to_sender += f"Kicked {kicked_user.username} from {room_name}. That'll show them."
+        send_to_others += f"{kicked_user.username} was kicked from the room. Let this be a lesson to you all."
+
+    if send_to_sender:
+        send_chat_message("\n".join(send_to_sender), get_group_name(GroupTypes.Line, user.username))
+    if send_to_others:
+        send_chat_message("\n".join(send_to_others), room_group)
 
 def ban(args, user, room_name):
     """
@@ -341,19 +360,19 @@ def ban(args, user, room_name):
     """
 
     if len(args) == 0:
-        send("Usage: /ban <user1> <user2> ...", client)
+        send_chat_message("Usage: /ban <user1> <user2> ...", client)
 
         return False
 
     if client.room is None:
-        send("Not in a room", client)
+        send_chat_message("Not in a room", client)
 
         return False
 
     # Check privileges
     if client.username != client.room.owner:
         if client.username not in client.room.admins:
-            send("Insufficient privileges to ban from: " +
+            send_chat_message("Insufficient privileges to ban from: " +
                       client.room.name, client)
 
             return False
@@ -363,13 +382,13 @@ def ban(args, user, room_name):
     for username in args:
 
         if username not in self.usernames and username not in self.passwords:
-            send(f"User does not exist: {username}", client)
+            send_chat_message(f"User does not exist: {username}", client)
 
             no_errors = False
             continue
 
         if username in client.room.banned:
-            send(f"User already banned: {username}", client)
+            send_chat_message(f"User already banned: {username}", client)
 
             no_errors = False
             continue
@@ -377,7 +396,7 @@ def ban(args, user, room_name):
         # Must be owner to ban admin
         if username in client.room.admins:
             if client.username != client.room.owner:
-                send("Insufficient privileges to ban admin: " +
+                send_chat_message("Insufficient privileges to ban admin: " +
                           f"{username}", client)
 
                 no_errors = False
@@ -385,14 +404,14 @@ def ban(args, user, room_name):
 
         # Do not allow users to ban themselves
         if username == client.username:
-            send("Cannot ban self", client)
+            send_chat_message("Cannot ban self", client)
 
             no_errors = False
             continue
 
         # Owner cannot be banned
         if username == client.room.owner:
-            send(f"Cannot ban owner: {client.room.owner}",
+            send_chat_message(f"Cannot ban owner: {client.room.owner}",
                       client)
 
             no_errors = False
@@ -411,10 +430,10 @@ def ban(args, user, room_name):
 
         # Notify all parties that a user was banned
         if username in self.usernames:
-            send(f"You were banned from: {client.room.name}",
+            send_chat_message(f"You were banned from: {client.room.name}",
                       user)
 
-        send(f"Banned user: {username}", client)
+        send_chat_message(f"Banned user: {username}", client)
 
         self.distribute(f"{username} was banned",
                         [client.room.name], None, [client])
@@ -437,19 +456,19 @@ def lift_ban(args, user, room_name):
     """
 
     if len(args) == 0:
-        send("Usage: /lift <user1> <user2> ...", client)
+        send_chat_message("Usage: /lift <user1> <user2> ...", client)
 
         return False
 
     if client.room is None:
-        send("Not in a room", client)
+        send_chat_message("Not in a room", client)
 
         return False
 
     # Check privileges
     if client.username != client.room.owner:
         if client.username not in client.room.admins:
-            send("Insufficient privileges to unban in: " +
+            send_chat_message("Insufficient privileges to unban in: " +
                       client.room.name, client)
 
             return False
@@ -459,13 +478,13 @@ def lift_ban(args, user, room_name):
     for username in args:
 
         if username not in self.usernames and username not in self.passwords:
-            send(f"User does not exist: {username}", client)
+            send_chat_message(f"User does not exist: {username}", client)
 
             no_errors = False
             continue
 
         if username not in client.room.banned:
-            send(f"User not banned: {username}", client)
+            send_chat_message(f"User not banned: {username}", client)
 
             no_errors = False
             continue
@@ -473,7 +492,7 @@ def lift_ban(args, user, room_name):
         # Must be owner to lift ban on admin
         if username in client.room.admins:
             if client.username != client.room.owner:
-                send("Insufficient privileges to unban admin: " +
+                send_chat_message("Insufficient privileges to unban admin: " +
                           f"{username}", client)
 
                 no_errors = False
@@ -483,10 +502,10 @@ def lift_ban(args, user, room_name):
 
         # Notify all parties that a user was banned
         if username in self.usernames:
-            send(f"Your were unbanned from: {client.room.name}",
+            send_chat_message(f"Your were unbanned from: {client.room.name}",
                       self.usernames[username])
 
-        send(f"Unbanned user: {username}", client)
+        send_chat_message(f"Unbanned user: {username}", client)
 
         self.distribute(f"{username} was unbanned",
                         [client.room.name], None, [client])
@@ -509,7 +528,7 @@ def delete_room(args, user, room_name):
     """
 
     if len(args) > 1:
-        send("Room name cannot contain spaces", client)
+        send_chat_message("Room name cannot contain spaces", client)
 
         return False
 
@@ -517,12 +536,12 @@ def delete_room(args, user, room_name):
         args.append(client.room.name)
 
     if args[0] not in self.rooms:
-        send(f"Room does not exist: {args[0]}", client)
+        send_chat_message(f"Room does not exist: {args[0]}", client)
 
         return False
 
     if client.username != self.rooms[args[0]].owner:
-        send(f"Insufficient privileges to delete: {args[0]}", client)
+        send_chat_message(f"Insufficient privileges to delete: {args[0]}", client)
 
         return False
 
@@ -534,10 +553,10 @@ def delete_room(args, user, room_name):
         user.typing = ""
 
         if user.username != room.owner:
-            send(f"The room was deleted: {room.name}", user)
+            send_chat_message(f"The room was deleted: {room.name}", user)
 
     del self.rooms[args[0]]
 
-    send(f"Deleted room: {args[0]}", client)
+    send_chat_message(f"Deleted room: {args[0]}", client)
 
     return True
--- OBChat/OB/consumers.py
+++ OBChat/OB/consumers.py
@@ -3,6 +3,7 @@ from enum import Enum
 
 from asgiref.sync import async_to_sync
 from channels.generic.websocket import WebsocketConsumer
+from .models import Room
 
 # TODO: Move these around so they're more organized
 class GroupTypes(Enum):
@@ -11,7 +12,7 @@ class GroupTypes(Enum):
     Room = 2
     Private = 3
 
-def group_name(group_type, name, second_name=""):
+def get_group_name(group_type, name, second_name=""):
     switch = {
         GroupTypes.Invalid: name,
         GroupTypes.Line: f"OBLine_{name}",
@@ -21,24 +22,31 @@ def group_name(group_type, name, second_name=""):
 
     return switch[group_type]
 
+class SystemOperations(Enum):
+    Invalid = 0
+    Kick = 1
+
 class ChatConsumer(WebsocketConsumer):
+    user = None
     room_name = None
     room_group_name = None
 
     def connect(self):
+        self.user = self.scope["user"]
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
 
         if self.room_name == "OBLine":
-            # TODO: Get username from url
-            self.room_group_name = group_name(GroupTypes.Line, "username")
+            self.room_group_name = get_group_name(GroupTypes.Line, self.user.username)
         else:
-            self.room_group_name = group_name(GroupTypes.Room, self.room_name)
+            self.room_group_name = get_group_name(GroupTypes.Room, self.room_name)
 
         # Join room group
-        async_to_sync(self.channel_layer.group_add) (
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
+
+        # TODO: add the user to the room's list of current users
 
         self.accept()
 
@@ -58,16 +66,20 @@ class ChatConsumer(WebsocketConsumer):
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                "type": "receive_from_group",
+                "type": "chat_message",
                 "message": message
             }
         )
 
     # Receive messages from room group
-    def receive_from_group(self, event):
+    def chat_message(self, event):
         message = event["message"]
 
         # Send message to WebSocket
         self.send(text_data=json.dumps({
             "message": message
         }))
+
+    def system_operation(self, event):
+        if event["operation"] == SystemOperations.Kick and event["user"] == self.user:
+            self.close()
