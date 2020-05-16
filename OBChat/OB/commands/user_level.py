ba6cde37dfd4d42b9cc17466b00b2b0dbe17b2f5
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-05-15 19:17:11 -0700

    Fixed /private command errors

--- OBChat/OB/commands/user_level.py
+++ OBChat/OB/commands/user_level.py
@@ -95,8 +95,8 @@ async def private(args, sender, room):
         recipient_object = await sync_try_get(OBUser, username=args[0][1:])
         # Check for per-argument errors
         if not recipient_object:
-            error_message = f"\"{args[0]}\" doesn't exist. Your private message will broadcasted \
-                into space instead."
+            error_message = (f"{args[0]} doesn't exist. Your private message will broadcasted "
+                            "into space instead.")
         elif len(args) == 1:
             error_message = "No message specified. Did you give up at just the username?"
 
