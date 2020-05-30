73e428ea872d2b4ba5c8ab7c412058f8eea36ec1
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-05-29 20:05:34 -0700

    Moved create_room() function to auth_user_level.py

---
+++ OBChat/OB/commands/auth_user_level.py
@@ -0,0 +1,51 @@
+"""
+A user must have be authenticated to perform these commands (see OB.models.Admin &
+OB.utilities.command.get_privilege()).
+"""
+
+from OB.constants import GroupTypes
+from OB.models import Room
+from OB.utilities.database import sync_save, sync_try_get
+from OB.utilities.event import send_system_room_message
+
+async def create_room(args, sender, room):
+    """
+    Description:
+        Create a new chat room from a commandline instead of through the website GUI.
+
+    Arguments:
+        args (list[string]): The desired name of the new room.
+        sender (OBUser): The OBUser who issued the command.
+        room (Room): The Room the command was sent from.
+    """
+
+    error_message = ""
+
+    # Check for errors
+    if not args:
+        error_message = "Usage: /room <name>"
+    elif not sender.is_authenticated or sender.is_anon:
+        error_message = "Identify yourself! Must log in to create a room."
+    elif len(args) > 1:
+        error_message = "Room name cannot contain spaces."
+    elif await sync_try_get(Room, group_type=GroupTypes.Room, name=args[0].lower()):
+        error_message = f"Someone beat you to it. {args[0].lower()} already exists."
+
+    # Send error message back to issuing user
+    if error_message:
+        await send_system_room_message(error_message, room, sender)
+        return
+
+    display_name = args[0] if not args[0].islower() else None
+
+    # Save the new room
+    created_room = await sync_save(
+        Room,
+        name=args[0].lower(),
+        owner=sender,
+        display_name=display_name
+    )
+
+    # Send success message back to issueing user
+    success_message = f"Sold! Check out your new room: {created_room}"
+    await send_system_room_message(success_message, room, sender)
