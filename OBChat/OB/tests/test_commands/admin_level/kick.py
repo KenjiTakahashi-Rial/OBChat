101214bcf352d9316f0924153e0c210e8923ac89
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-06-11 18:01:33 -0700

    Changed /kick testing to use OBCommunicator.send() instead of handle_command()

--- OBChat/OB/tests/test_commands/admin_level/kick.py
+++ OBChat/OB/tests/test_commands/admin_level/kick.py
@@ -33,7 +33,6 @@ async def test_kick():
         await database_setup()
 
         # Get database objects
-        owner = await async_get(OBUser, username="owner")
         unlimited_admin_0 = await async_get(OBUser, username="unlimited_admin_0")
         limited_admin_0 = await async_get(OBUser, username="limited_admin_0")
         auth_user = await async_get(OBUser, username="auth_user")
@@ -45,63 +44,80 @@ async def test_kick():
         communicators = await communicator_setup(room_0)
 
         # Test unauthenticated user kicking error
-        await handle_command("/kick", anon_0, room_0)
+        message = "/kick"
         correct_response = (
             "You're not even logged in! Try making an account first, then we can talk about "
             "kicking people."
         )
+        await communicators[f"{ANON_PREFIX}0"].send(message)
+        assert await communicators[f"{ANON_PREFIX}0"].receive() == message
         assert await communicators[f"{ANON_PREFIX}0"].receive() == correct_response
 
         # Test authenticated user kicking error
-        await handle_command("/k", auth_user, room_0)
+        message = "/k"
         correct_response = (
             "That's a little outside your pay-grade. Only admins may kick users. "
             "Try to /apply to be an admin."
         )
+        await communicators["auth_user"].send(message)
+        assert await communicators["auth_user"].receive() == message
         assert await communicators["auth_user"].receive() == correct_response
 
         # Test no arguments error
-        await handle_command("/k", limited_admin_0, room_0)
+        message = "/k"
         correct_response = "Usage: /kick <user1> <user2> ..."
+        await communicators["limited_admin_0"].send(message)
+        assert await communicators["limited_admin_0"].receive() == message
         assert await communicators["limited_admin_0"].receive() == correct_response
 
         # Test absent target error
-        await handle_command("/k auth_user_1", limited_admin_0, room_0)
+        message = "/k auth_user_1"
         correct_response = "Nobody named auth_user_1 in this room. Are you seeing things?"
+        await communicators["limited_admin_0"].send(message)
+        assert await communicators["limited_admin_0"].receive() == message
         assert await communicators["limited_admin_0"].receive() == correct_response
 
         # Test self target error
-        await handle_command("/k limited_admin_0", limited_admin_0, room_0)
+        message = "/k limited_admin_0"
         correct_response = (
             "You can't kick yourself. Just leave the room. Or put yourself on time-out."
         )
+        await communicators["limited_admin_0"].send(message)
+        assert await communicators["limited_admin_0"].receive() == message
         assert await communicators["limited_admin_0"].receive() == correct_response
 
         # Test limited admin kicking owner error
-        await handle_command("/k owner", limited_admin_0, room_0)
+        message = "/k owner"
         correct_response = "That's the owner. You know, your BOSS. Nice try."
+        await communicators["limited_admin_0"].send(message)
+        assert await communicators["limited_admin_0"].receive() == message
         assert await communicators["limited_admin_0"].receive() == correct_response
 
         # Test limited admin kicking unlimited admin error
-        await handle_command("/k unlimited_admin_0", limited_admin_0, room_0)
+        message = "/k unlimited_admin_0"
         correct_response = (
             "unlimited_admin_0 is an unlimited admin, so you can't kick them. Please direct all "
             "complaints to your local room owner, I'm sure they'll love some more paperwork to "
             "do..."
         )
+        await communicators["limited_admin_0"].send(message)
+        assert await communicators["limited_admin_0"].receive() == message
         assert await communicators["limited_admin_0"].receive() == correct_response
 
         # Test limited admin kicking limited admin error
-        await handle_command("/k limited_admin_1", limited_admin_0, room_0)
+        message = "/k limited_admin_1"
         correct_response = (
             "limited_admin_1 is an admin just like you, so you can't kick them. Please direct all "
             "complaints to your local room owner, I'm sure they'll love some more paperwork to "
             "do..."
         )
+        await communicators["limited_admin_0"].send(message)
+        assert await communicators["limited_admin_0"].receive() == message
         assert await communicators["limited_admin_0"].receive() == correct_response
 
         # Test limited admin kicking authenticated user
-        await handle_command("/k auth_user", limited_admin_0, room_0)
+        message = "/k auth_user"
+        await communicators["limited_admin_0"].send(message)
         sender_response = (
             "Kicked:\n"
             "   auth_user\n"
@@ -112,7 +128,7 @@ async def test_kick():
             "   auth_user\n"
             "Let this be a lesson to you all."
         )
-        assert auth_user not in await async_model_list(room_0.occupants)
+        assert await communicators["limited_admin_0"].receive() == message
         assert await communicators["limited_admin_0"].receive() == sender_response
         assert (
             await communicators["owner"].receive() ==
@@ -125,6 +141,7 @@ async def test_kick():
         )
         assert (await communicators["auth_user"].receive())["refresh"]
         assert (await communicators["auth_user"].receive_output())["type"] == "websocket.close"
+        assert auth_user not in await async_model_list(room_0.occupants)
 
         # Add kicked users back to room occupants and reset Communicators
         await communicator_teardown(communicators)
@@ -140,21 +157,26 @@ async def test_kick():
         communicators = await communicator_setup(room_0)
 
         # Test unlimited admin kicking owner error
-        await handle_command("/k owner", unlimited_admin_0, room_0)
+        message = "/k owner"
         correct_response = "That's the owner. You know, your BOSS. Nice try."
+        await communicators["unlimited_admin_0"].send(message)
+        assert await communicators["unlimited_admin_0"].receive() == message
         assert await communicators["unlimited_admin_0"].receive() == correct_response
 
         # Test unlimited admin kicking unlimited admin error
-        await handle_command("/k unlimited_admin_1", unlimited_admin_0, room_0)
+        message = "/k unlimited_admin_1"
         correct_response = (
             "unlimited_admin_1 is an unlimited admin just like you, so you can't kick them. Please"
             " direct all complaints to your local room owner, I'm sure they'll love some more "
             "paperwork to do..."
         )
+        await communicators["unlimited_admin_0"].send(message)
+        assert await communicators["unlimited_admin_0"].receive() == message
         assert await communicators["unlimited_admin_0"].receive() == correct_response
 
         # Test unlimited admin kicking limited admin
-        await handle_command("/k limited_admin_0", unlimited_admin_0, room_0)
+        message = "/k limited_admin_0"
+        await communicators["unlimited_admin_0"].send(message)
         sender_response = (
             "Kicked:\n"
             "   limited_admin_0\n"
@@ -165,7 +187,8 @@ async def test_kick():
             "   limited_admin_0\n"
             "Let this be a lesson to you all."
         )
-        assert limited_admin_0 not in await async_model_list(room_0.occupants)
+
+        assert await communicators["unlimited_admin_0"].receive() == message
         assert await communicators["unlimited_admin_0"].receive() == sender_response
         assert (
             await communicators["owner"].receive() ==
@@ -180,9 +203,11 @@ async def test_kick():
         assert (
             (await communicators["limited_admin_0"].receive_output())["type"] == "websocket.close"
         )
+        assert limited_admin_0 not in await async_model_list(room_0.occupants)
 
         # Test unlimited admin kicking authenticated user
-        await handle_command("/k auth_user", unlimited_admin_0, room_0)
+        message = "/k auth_user"
+        await communicators["unlimited_admin_0"].send(message)
         sender_response = (
             "Kicked:\n"
             "   auth_user\n"
@@ -193,7 +218,7 @@ async def test_kick():
             "   auth_user\n"
             "Let this be a lesson to you all."
         )
-        assert auth_user not in await async_model_list(room_0.occupants)
+        assert await communicators["unlimited_admin_0"].receive() == message
         assert await communicators["unlimited_admin_0"].receive() == sender_response
         assert (
             await communicators["owner"].receive() ==
@@ -205,6 +230,7 @@ async def test_kick():
         )
         assert (await communicators["auth_user"].receive())["refresh"]
         assert (await communicators["auth_user"].receive_output())["type"] == "websocket.close"
+        assert auth_user not in await async_model_list(room_0.occupants)
 
         # Add kicked users back to room occupants and reset Communicators
         await communicator_teardown(communicators)
@@ -220,12 +246,9 @@ async def test_kick():
         communicators = await communicator_setup(room_0)
 
         # Test owner kicking multiple users
-        await handle_command(
-            # TODO: Testing kicking anonymous users is causing database lock
-            f"/k unlimited_admin_0 limited_admin_0 auth_user", #{ANON_PREFIX}0",
-            owner,
-            room_0
-        )
+        # TODO: Testing kicking anonymous users is causing database lock
+        message = f"/k unlimited_admin_0 limited_admin_0 auth_user" #, {ANON_PREFIX}0",
+        await communicators["owner"].send(message)
         sender_response = (
             "Kicked:\n"
             "   unlimited_admin_0\n"
@@ -242,10 +265,7 @@ async def test_kick():
             # f"   {ANON_PREFIX}0\n"
             "Let this be a lesson to you all."
         )
-        assert unlimited_admin_0 not in await async_model_list(room_0.occupants)
-        assert limited_admin_0 not in await async_model_list(room_0.occupants)
-        assert auth_user not in await async_model_list(room_0.occupants)
-        # assert anon_0 not in await async_model_list(room_0.occupants)
+        assert await communicators["owner"].receive() == message
         assert await communicators["owner"].receive() == sender_response
         assert (
             await communicators["owner"].receive() ==
@@ -267,6 +287,10 @@ async def test_kick():
         # assert (
         #     (await communicators[f"{ANON_PREFIX}0"].receive_output())["type"] == "websocket.close"
         # )
+        assert unlimited_admin_0 not in await async_model_list(room_0.occupants)
+        assert limited_admin_0 not in await async_model_list(room_0.occupants)
+        assert auth_user not in await async_model_list(room_0.occupants)
+        # assert anon_0 not in await async_model_list(room_0.occupants)
 
     finally:
         await communicator_teardown(communicators)
