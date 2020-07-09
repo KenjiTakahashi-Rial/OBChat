b1122103a26ad1649392ed98fb3bbe7d85aba581
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-07-08 19:05:27 -0700

    Shortened KickTest.tests() code by using BaseCommandTest.test_isolated()

--- OBChat/OB/tests/test_commands/admin_level/kick.py
+++ OBChat/OB/tests/test_commands/admin_level/kick.py
@@ -29,97 +29,11 @@ class KickTest(BaseCommandTest):
         """
         Description:
             Tests the /kick command (see OB.commands.user_level.kick()).
+            Tests errors last just in case previous tests fail and the test must run again.
         """
-
-        # Test unauthenticated user kicking error
-        message = "/kick"
-        correct_response = (
-            "You're not even logged in! Try making an account first, then we can talk about "
-            "kicking people."
-        )
-        await self.communicators[f"{ANON_PREFIX}0"].send(message)
-        assert await self.communicators[f"{ANON_PREFIX}0"].receive() == message
-        assert await self.communicators[f"{ANON_PREFIX}0"].receive() == correct_response
-
-        # Test authenticated user kicking error
-        message = "/k"
-        correct_response = (
-            "That's a little outside your pay-grade. Only admins may kick users. "
-            "Try to /apply to be an admin."
-        )
-        await self.communicators["auth_user_0"].send(message)
-        assert await self.communicators["auth_user_0"].receive() == message
-        assert await self.communicators["auth_user_0"].receive() == correct_response
-
-        # Test no arguments error
-        message = "/k"
-        correct_response = "Usage: /kick <user1> <user2> ..."
-        await self.communicators["limited_admin_0"].send(message)
-        assert await self.communicators["limited_admin_0"].receive() == message
-        assert await self.communicators["limited_admin_0"].receive() == correct_response
-
-        # Test absent target error
-        message = "/k auth_user_0_1"
-        correct_response = "Nobody named auth_user_0_1 in this room. Are you seeing things?"
-        await self.communicators["limited_admin_0"].send(message)
-        assert await self.communicators["limited_admin_0"].receive() == message
-        assert await self.communicators["limited_admin_0"].receive() == correct_response
-
-        # Test self target error
-        message = "/k limited_admin_0"
-        correct_response = (
-            "You can't kick yourself. Just leave the room. Or put yourself on time-out."
-        )
-        await self.communicators["limited_admin_0"].send(message)
-        assert await self.communicators["limited_admin_0"].receive() == message
-        assert await self.communicators["limited_admin_0"].receive() == correct_response
-
-        # Test limited admin kicking owner error
-        message = "/k owner"
-        correct_response = "That's the owner. You know, your BOSS. Nice try."
-        await self.communicators["limited_admin_0"].send(message)
-        assert await self.communicators["limited_admin_0"].receive() == message
-        assert await self.communicators["limited_admin_0"].receive() == correct_response
-
-        # Test limited admin kicking unlimited admin error
-        message = "/k unlimited_admin_0"
-        correct_response = (
-            f"{self.unlimited_admins[0]} is an unlimited admin, so you can't kick them. Feel free "
-            "to /elevate your complaints to someone who has more authority."
-        )
-        await self.communicators["limited_admin_0"].send(message)
-        assert await self.communicators["limited_admin_0"].receive() == message
-        assert await self.communicators["limited_admin_0"].receive() == correct_response
-
-        # Test limited admin kicking limited admin error
-        message = "/k limited_admin_1"
-        correct_response = (
-            f"{self.limited_admins[1]} is an admin just like you, so you can't kick them. Feel "
-            "free to /elevate your complaints to someone who has more authority."
-        )
-        await self.communicators["limited_admin_0"].send(message)
-        assert await self.communicators["limited_admin_0"].receive() == message
-        assert await self.communicators["limited_admin_0"].receive() == correct_response
 
         # Test limited admin kicking authenticated user
         await self.test_success(self.limited_admins[0], [self.auth_users[0]])
-
-        # Test unlimited admin kicking owner error
-        message = "/k owner"
-        correct_response = "That's the owner. You know, your BOSS. Nice try."
-        await self.communicators["unlimited_admin_0"].send(message)
-        assert await self.communicators["unlimited_admin_0"].receive() == message
-        assert await self.communicators["unlimited_admin_0"].receive() == correct_response
-
-        # Test unlimited admin kicking unlimited admin error
-        message = "/k unlimited_admin_1"
-        correct_response = (
-            f"{self.unlimited_admins[1]} is an unlimited admin just like you, so you can't kick "
-            "them. Feel free to /elevate your complaints to someone who has more authority."
-        )
-        await self.communicators["unlimited_admin_0"].send(message)
-        assert await self.communicators["unlimited_admin_0"].receive() == message
-        assert await self.communicators["unlimited_admin_0"].receive() == correct_response
 
         # Test unlimited admin kicking limited admin
         await self.test_success(self.unlimited_admins[0], [self.limited_admins[0]])
@@ -139,6 +53,73 @@ class KickTest(BaseCommandTest):
             self.owner,
             [self.unlimited_admins[0], self.limited_admins[0], self.auth_users[0]]
         )
+
+        # Test unauthenticated user kicking error
+        message = "/kick"
+        correct_response = (
+            "You're not even logged in! Try making an account first, then we can talk about "
+            "kicking people."
+        )
+        await self.test_isolated(self.anon_users[0], message, correct_response)
+
+        # Test authenticated user kicking error
+        message = "/k"
+        correct_response = (
+            "That's a little outside your pay-grade. Only admins may kick users. "
+            "Try to /apply to be an admin."
+        )
+        await self.test_isolated(self.auth_users[0], message, correct_response)
+
+        # Test no arguments error
+        message = "/k"
+        correct_response = "Usage: /kick <user1> <user2> ..."
+        await self.test_isolated(self.limited_admins[0], message, correct_response)
+
+        # Test absent target error
+        message = "/k auth_user_0_1"
+        correct_response = "Nobody named auth_user_0_1 in this room. Are you seeing things?"
+        await self.test_isolated(self.limited_admins[0], message, correct_response)
+
+        # Test self target error
+        message = "/k limited_admin_0"
+        correct_response = (
+            "You can't kick yourself. Just leave the room. Or put yourself on time-out."
+        )
+        await self.test_isolated(self.limited_admins[0], message, correct_response)
+
+        # Test limited admin kicking owner error
+        message = "/k owner"
+        correct_response = "That's the owner. You know, your BOSS. Nice try."
+        await self.test_isolated(self.limited_admins[0], message, correct_response)
+
+        # Test limited admin kicking unlimited admin error
+        message = "/k unlimited_admin_0"
+        correct_response = (
+            f"{self.unlimited_admins[0]} is an unlimited admin, so you can't kick them. Feel free "
+            "to /elevate your complaints to someone who has more authority."
+        )
+        await self.test_isolated(self.limited_admins[0], message, correct_response)
+
+        # Test limited admin kicking limited admin error
+        message = "/k limited_admin_1"
+        correct_response = (
+            f"{self.limited_admins[1]} is an admin just like you, so you can't kick them. Feel "
+            "free to /elevate your complaints to someone who has more authority."
+        )
+        await self.test_isolated(self.limited_admins[0], message, correct_response)
+
+        # Test unlimited admin kicking owner error
+        message = "/k owner"
+        correct_response = "That's the owner. You know, your BOSS. Nice try."
+        await self.test_isolated(self.unlimited_admins[0], message, correct_response)
+
+        # Test unlimited admin kicking unlimited admin error
+        message = "/k unlimited_admin_1"
+        correct_response = (
+            f"{self.unlimited_admins[1]} is an unlimited admin just like you, so you can't kick "
+            "them. Feel free to /elevate your complaints to someone who has more authority."
+        )
+        await self.test_isolated(self.unlimited_admins[0], message, correct_response)
 
     @mark.asyncio
     @mark.django_db()
