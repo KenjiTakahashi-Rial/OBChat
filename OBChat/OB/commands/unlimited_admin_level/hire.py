0c24847ed5827d9c0d0abaafdc87cb087d038840
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-07-11 10:58:42 -0700

    Created some tests for /hire

--- OBChat/OB/commands/unlimited_admin_level/hire.py
+++ OBChat/OB/commands/unlimited_admin_level/hire.py
@@ -6,7 +6,7 @@ A user must have unlimited admin privileges of the room to perform this command
 from OB.constants import Privilege
 from OB.models import Admin, OBUser
 from OB.utilities.command import async_get_privilege
-from OB.utilities.database import async_save, try_get
+from OB.utilities.database import async_save, async_try_get
 from OB.utilities.event import send_system_room_message
 
 async def hire(args, sender, room):
@@ -21,56 +21,60 @@ async def hire(args, sender, room):
         room (Room): The Room the command was sent from.
     """
 
+    # Remove duplicates
+    args = list(dict.fromkeys(args))
+
+    error_message = ""
     sender_privilege = await async_get_privilege(sender, room)
 
     # Check for initial errors
     if sender_privilege < Privilege.UnlimitedAdmin:
         error_message = (
-            "That's a little outside your pay-grade. Only unlimited admins may hire admins. Try to"
+            "That's a little outside your pay-grade. Only Unlimited Admins may hire admins. Try to"
             " /apply to be unlimited."
         )
     elif len(args) == 0:
         error_message = "Usage: /hire <user1> <user2> ..."
 
     if error_message:
-        await send_system_room_message(error_message, room)
+        await send_system_room_message(error_message, room, [sender])
         return
 
     valid_hires = []
     error_messages = []
 
     for username in args:
-        arg_user = try_get(OBUser, username=username)
-        arg_admin = try_get(Admin, user=arg_user)
+        arg_user = await async_try_get(OBUser, username=username)
+        arg_admin = await async_try_get(Admin, user=arg_user)
 
         # Check for per-argument errors
         if not arg_user:
-            error_messages += (
+            error_messages += [
                 f"{username} does not exist. Your imaginary friend needs an account before "
-                "they can be an admin."
-            )
+                "they can be an Admin."
+            ]
         elif arg_user == sender:
-            error_messages += (
+            error_messages += [
                 f"You can't hire yourself. I don't care how good your letter of recommendation is."
-            )
+            ]
         elif arg_user == room.owner:
             error_messages += f"That's the owner. You know, your BOSS. Nice try."
         elif not sender.is_authenticated:
-            error_messages += (
+            error_messages += [
                 f"{username} hasn't signed up yet. They cannot be trusted with the immense "
                 "responsibility that is adminship."
-            )
+            ]
         elif arg_admin:
             if not arg_admin.is_limited:
-                error_message += (
+                error_message += [
                     f"{username} is already an Unlimited Admin. There's nothing left to /hire them"
                     " for."
-                )
+                ]
             elif sender_privilege < Privilege.Owner:
-                error_message += (
+                error_message += [
                     f"{username} is already an admin. Only the owner may promote them to Unlimited"
                     " Admin."
-                )
+                ]
         else:
             valid_hires += arg_user
 
