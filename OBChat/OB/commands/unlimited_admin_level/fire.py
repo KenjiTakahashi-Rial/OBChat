e13823966681e196978e21c372a5b3d3351adf98
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-07-17 19:29:17 -0700

    Changed /fire to work more like /hire

--- OBChat/OB/commands/unlimited_admin_level/fire.py
+++ OBChat/OB/commands/unlimited_admin_level/fire.py
@@ -1,12 +1,12 @@
 """
-A user must have Unlimited Admin privileges of the room to perform this command
-(see OB.models.Admin & OB.constants.Privilege).
+A user must have Unlimited Admin privileges of the room to perform this command (see
+OB.models.Admin & OB.constants.Privilege).
 """
 
 from OB.constants import Privilege
 from OB.models import Admin, OBUser
 from OB.utilities.command import async_get_privilege
-from OB.utilities.database import try_get
+from OB.utilities.database import async_delete, async_save, async_try_get
 from OB.utilities.event import send_system_room_message
 
 async def fire(args, sender, room):
@@ -21,8 +21,14 @@ async def fire(args, sender, room):
         room (Room): The Room the command was sent from.
     """
 
+    # Remove duplicates
+    args = list(dict.fromkeys(args))
+
+    error_message = ""
+    sender_privilege = await async_get_privilege(sender, room)
+
     # Check for initial errors
-    if await async_get_privilege(sender, room) < Privilege.UnlimitedAdmin:
+    if sender_privilege < Privilege.UnlimitedAdmin:
         error_message = (
             "That's a little outside your pay-grade. Only Unlimited Admins may fire admins. Try to"
             " /apply to be Unlimited."
@@ -39,48 +45,61 @@ async def fire(args, sender, room):
     error_messages = []
 
     for username in args:
-        arg_user = try_get(OBUser, username=username)
-        arg_admin = try_get(Admin, user=arg_user)
+        arg_user = await async_try_get(OBUser, username=username)
+        arg_admin = await async_try_get(Admin, user=arg_user)
 
         # Check for per-argument errors
         if not arg_user:
-            error_messages += (
-                "\"{username}\" does not exist. You can't fire a ghost... can you?"
-            )
+            error_messages += [f"{username} does not exist. You can't fire a ghost... can you?"]
         elif arg_user == sender:
-            error_messages += (
+            error_messages += [
                 "You can't fire yourself. I don't care how bad your performance reviews are."
-            )
+            ]
         elif arg_user == room.owner:
-            error_messages += f"That's the owner. You know, your BOSS. Nice try."
+            error_messages += ["That's the owner. You know, your BOSS. Nice try."]
         elif not arg_admin:
-            error_messages += (
-                f"\"{username}\" is just a regular ol' user, so you can't fire them. You can /ban "
-                "them if you want."
-            )
+            error_messages += [
+                f"{username} is just a regular ol' user, so you can't fire them. You can /kick or "
+                "/ban them if you want."
+            ]
         elif sender != room.owner and not arg_admin.is_limited:
-            error_messages += (
-                f"\"{username}\" is an Unlimited Admin, so you can't fire them. Please direct all "
+            error_messages += [
+                f"{username} is an Unlimited Admin, so you can't fire them. Please direct all "
                 "complaints to your local room owner, I'm sure they'll love some more paperwork to"
-                " do...")
+                " do..."
+            ]
         else:
-            valid_fires += (arg_user, arg_admin)
+            valid_fires += [{"user": arg_user, "adminship": arg_admin}]
 
-    # Remove admin(s) and notify all parties that a user was fired
-    send_to_sender = error_messages
-    send_to_others = []
+    send_to_sender = error_messages + [("\n" if error_messages else "") + "Fired:"]
+    send_to_targets = ["One or more users have been fired:"] # TODO: Change this to only send when more than one user is fired at once
+    send_to_others = ["One or more users have been fired:"]
 
-    # Fire valid fires (if any) and notify all parties that a user was fired
     for fired_user in valid_fires:
-        fired_user[1].delete()
+        if fired_user["adminship"].is_limited:
+            # Remove the adminship
+            await async_delete(fired_user["adminship"])
+        else:
+            # Make the adminship limited
+            fired_user["adminship"].is_limited = True
+            await async_save(fired_user["adminship"])
 
-        await send_system_room_message(f"Clean out your desk. You lost your adminship at "
-                                       "\"{room.name}\".", room)
+        send_to_sender += [f"    {fired_user}"]
+        send_to_targets += [f"    {fired_user}"]
+        send_to_others += [f"    {fired_user}"]
 
-        send_to_sender += f"It had to be done. You fired \"{fired_user[0].username}\""
-        send_to_others += f"{fired_user[0].username} was fired! Those budget cuts are killer."
-
-    if send_to_sender:
+    if valid_fires:
+        send_to_sender += ["It had to be done."]
         await send_system_room_message("\n".join(send_to_sender), room, [sender])
-    if send_to_others:
-        await send_system_room_message("\n".join(send_to_others), room)
+
+        send_to_targets += ["Clean out your desk."]
+        await send_system_room_message("\n".join(send_to_sender), room, valid_fires)
+
+        send_to_others += ["Those budget cuts are killer."]
+        await send_system_room_message(
+            "\n".join(send_to_others),
+            room,
+            exclusions=([sender] + valid_fires)
+        )
+    elif error_messages:
+        await send_system_room_message("\n".join(error_messages), room, [sender])
