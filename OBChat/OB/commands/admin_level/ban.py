93d5d81ce39708a89299d768afe035cda5517885
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-07-31 19:15:38 -0700

    Modified OB.commands.admin_level.ban to use inherit from BaseCommand class

--- OBChat/OB/commands/admin_level/ban.py
+++ OBChat/OB/commands/admin_level/ban.py
@@ -1,188 +1,151 @@
 """
-/ban command functions container module.
+BanCommand class container module.
 """
 
+from OB.commands.base import BaseCommand
 from OB.constants import Privilege
 from OB.models import Ban, OBUser
 from OB.utilities.command import async_get_privilege
-from OB.utilities.database import async_get_owner, async_save, async_try_get
-from OB.utilities.event import send_room_event, send_system_room_message
+from OB.utilities.database import async_save, async_try_get
+from OB.utilities.event import send_room_event
 
-async def ban(args, sender, room):
+class BanCommand(BaseCommand):
     """
-    Remove one or more OBConsumers from the group a Room is associated with and do not allow
-    them to rejoin the group. Bans may be lifted (see lift()).
-
-    Arguments:
-        args (list[string]): The usernames of OBUsers to ban. Should have length 1 or more.
-        sender (OBUser): The OBUser who issued the command.
-        room (Room): The Room the command was sent from.
+    Remove one or more OBConsumers from the group a Room is associated with and do not allow them
+    to rejoin the group. Bans may be lifted (see lift()).
     """
 
-    # Remove duplicates
-    args = list(dict.fromkeys(args))
+    def __init__(self, args, sender, room):
+        """
+        See BaseCommand.__init__().
+        """
 
-    # Check for initial errors
-    initial_error_message = await check_initial_errors(args, sender, room)
+        super().__init__(args, sender, room)
+        self.valid_bans = []
 
-    # Send initial error message back to user and cancel the command
-    if initial_error_message:
-        await send_system_room_message(initial_error_message, room, [sender])
-        return
+    async def execute(self):
+        """
+        The main implementation of the /ban command.
+        """
 
-    # Check the validity of the arguments
-    valid_bans, arg_error_messages = await check_arguments(args, sender, room)
+        # Remove duplicates
+        self.args = list(dict.fromkeys(self.args))
 
-    # Execute the bans
-    sender_receipt, occupants_notification = await execute_bans(
-        valid_bans,
-        arg_error_messages,
-        sender,
-        room
-    )
-
-    # Send messages if applicable
-    if sender_receipt:
-        await send_system_room_message(sender_receipt, room, [sender])
-
-    if occupants_notification:
-        await send_system_room_message(occupants_notification, room, exclusions=[sender])
-
-async def check_initial_errors(args, sender, room):
-    """
-    Check for initial errors such as lack of privilege or invalid syntax.
-
-    Arguments:
-        args (list[string]): The usernames of OBUsers to ban. Should have length 1 or more.
-        sender (OBUser): The OBUser who issued the command.
-        room (Room): The Room the command was sent from.
-
-    Return values:
-        string: Returns an appropriate error message string. If there are no errors, returns the
-            empty string.
-    """
-
-    error_message = ""
-    sender_privilege = await async_get_privilege(sender, room)
-
-    if sender_privilege < Privilege.AuthUser:
-        # Is an anonymous/unauthenticated user
-        error_message = (
-            "You're not even logged in! Try making an account first, then we can talk about "
-            "banning people."
-        )
-    elif sender_privilege < Privilege.Admin:
-        # Is authenticated, but not an admin
-        error_message = (
-            "That's a little outside your pay-grade. Only admins may ban users. Try to /apply to "
-            "be an Admin."
-        )
-    elif not args:
-        # Missing target arguments
-        error_message = "Usage: /ban <user1> <user2> ..."
-
-    return error_message
-
-async def check_arguments(args, sender, room):
-    """
-    Check each argument for errors such as self-targeting or targeting a user of higher privilege.
-
-    Arguments:
-        args (list[string]): The usernames of OBUsers to ban. Should have length 1 or more.
-        sender (OBUser): The OBUser who issued the command.
-        room (Room): The Room the command was sent from.
-
-    Return values:
-        tuple(list[OBUser], list[string]): Returns a tuple of a list of OBUsers to ban and a list
-            of error messages to send to the sender.
-    """
-
-    valid_bans = []
-    error_messages = []
-
-    for username in args:
-        arg_user = await async_try_get(OBUser, username=username)
-        arg_privilege = None if not arg_user else await async_get_privilege(arg_user, room)
-
-        if not arg_user:
-            # Target user does not exist
-            error_messages += [f"Nobody named {username} in this room. Are you seeing things?"]
-        elif arg_user == sender:
-            # Target user is the sender, themself
-            error_messages += [
-                f"You can't ban yourself. Just leave the room. Or put yourself on time-out."
-            ]
-        elif arg_user == await async_get_owner(room):
-            # Target user is the owner
-            error_messages += [f"That's the owner. You know, your BOSS. Nice try."]
-        elif arg_privilege >= await async_get_privilege(sender, room):
-            # Target user has Privilege greater than or equal to the sender
-            job_title = "Admin"
-
-            if arg_privilege == await async_get_privilege(sender, room):
-                job_title += " just like you"
-
-            if arg_privilege == Privilege.UnlimitedAdmin:
-                job_title = "Unlimited " + job_title
-
-            error_messages += [
-                f"{arg_user} is an {job_title}, so you can't ban them. Feel free to "
-                "/elevate your complaints to someone who has more authority."
-            ]
+        # Check for initial errors
+        if not await self.check_initial_errors():
+            pass
+        # Check the validity of the arguments
+        elif not await self.check_arguments():
+            pass
+        # Execute the bans
         else:
-            # Target user is a valid ban
-            valid_bans += [arg_user]
+            await self.execute_bans()
 
-    return valid_bans, error_messages
+        await self.send_responses()
 
-async def execute_bans(bans, arg_error_messages, sender, room):
-    """
-    Kick and ban users from the room.
-    Constructs strings to send back to the sender and to the other occupants of the room.
+    async def check_initial_errors(self):
+        """
+        Check for initial errors such as lack of privilege or invalid syntax.
+        If there are any errors, send them back to the issuing user.
 
-    Arguments:
-        bans (list[OBUser]): List of users to ban. Assumes that the validity of these users being
-            banned has already been checked.
-        arg_error_messages (list[string]): A list of argument error message strings (see
-            check_arguments()).
-        sender (OBUser): The OBUser who issued the command.
-        room (Room): The Room the command was sent from.
+        Return values:
+            boolean: True if there were no initial errors.
+        """
 
-    Return values:
-        tuple(string, string): Returns a tuple of a sender receipt string and a room occupant
-            notification string.
-    """
+        # Is an anonymous/unauthenticated user
+        if self.sender_privilege < Privilege.AuthUser:
+            self.sender_receipt = [
+                "You're not even logged in! Try making an account first, then we can talk about "
+                "banning people."
+            ]
+        # Is authenticated, but not an admin
+        elif self.sender_privilege < Privilege.Admin:
+            self.sender_receipt = [
+                "That's a little outside your pay-grade. Only admins may ban users. Try to /apply "
+                "to be an Admin."
+            ]
+        # Missing target arguments
+        elif not self.args:
+            self.sender_receipt = ["Usage: /ban <user1> <user2> ..."]
 
-    if not bans:
-        # Send the argument error messages back to the sender if there are any
-        # Do not create a notification for other occupants
-        return "\n".join(arg_error_messages) if arg_error_messages else "", ""
+        return not self.sender_receipt
 
-    # Prepend the argument error messages to the sender's receipt
-    sender_receipt = arg_error_messages + [("\n" if arg_error_messages else "") + "Banned:"]
-    occupants_notification = ["One or more users have been banned:"]
+    async def check_arguments(self):
+        """
+        Check each argument for errors such as self-targeting or targeting a user of higher
+        privilege.
 
-    for banned_user in bans:
-        # Save the ban to the database
-        await async_save(
-            Ban,
-            user=banned_user,
-            room=room,
-            issuer=sender
-        )
+        Return values:
+            boolean: True if any of the arguments are a valid ban.
+        """
 
-        # Kick the user
-        kick_event = {
-            "type": "kick",
-            "target_id": banned_user.id
-        }
+        for username in self.args:
+            arg_user = await async_try_get(OBUser, username=username)
+            arg_privilege = None if not arg_user else await async_get_privilege(arg_user, self.room)
 
-        await send_room_event(room.id, kick_event)
+            if not arg_user:
+                # Target user does not exist
+                error_messages += [f"Nobody named {username} in this room. Are you seeing things?"]
+            elif arg_user == self.sender:
+                # Target user is the sender, themself
+                error_messages += [
+                    f"You can't ban yourself. Just leave the room. Or put yourself on time-out."
+                ]
+            elif arg_privilege == Privilege.Owner:
+                # Target user is the owner
+                error_messages += [f"That's the owner. You know, your BOSS. Nice try."]
+            elif arg_privilege >= self.sender_privilege:
+                # Target user has Privilege greater than or equal to the sender
+                job_title = "Admin"
 
-        sender_receipt += [f"   {banned_user}"]
-        occupants_notification += [f"   {banned_user}"]
+                if arg_privilege == await async_get_privilege(self.sender, self.room):
+                    job_title += " just like you"
 
-    sender_receipt += ["That'll show them."]
-    occupants_notification += ["Let this be a lesson to you all."]
+                if arg_privilege == Privilege.UnlimitedAdmin:
+                    job_title = "Unlimited " + job_title
 
-    return "\n".join(sender_receipt), "\n".join(occupants_notification)
+                error_messages += [
+                    f"{arg_user} is an {job_title}, so you can't ban them. Feel free to "
+                    "/elevate your complaints to someone who has more authority."
+                ]
+            else:
+                # Target user is a valid ban
+                valid_bans += [arg_user]
+
+        self.sender_receipt += error_messages
+
+        return bool(self.valid_bans)
+
+    async def execute_bans(self):
+        """
+        Kick and ban users from the room.
+        Constructs strings to send back to the sender and to the other occupants of the room.
+        The sender receipt includes per-argument error messages.
+        """
+
+        # Prepend the argument error messages to the sender's receipt
+        self.sender_receipt += [("\n" if self.sender_receipt else "") + "Banned:"]
+        self.occupants_notification = ["One or more users have been banned:"]
+
+        for banned_user in self.valid_bans:
+            # Save the ban to the database
+            await async_save(
+                Ban,
+                user=banned_user,
+                room=self.room,
+                issuer=self.sender
+            )
+
+            # Kick the user
+            kick_event = {
+                "type": "kick",
+                "target_id": banned_user.id
+            }
+            await send_room_event(self.room.id, kick_event)
+
+            # Add the user's name to the sender receipt and occupants notification
+            self.sender_receipt += [f"   {banned_user}"]
+            self.occupants_notification += [f"   {banned_user}"]
+
+        self.sender_receipt += ["That'll show them."]
+        self.occupants_notification += ["Let this be a lesson to you all."]
