2d9e19faa724bbcd3b2a73d1cd44831ac596000d
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-08-03 19:28:09 -0700

    Modified OB.commands.admin_level.lift to use inherit from BaseCommand class

--- OBChat/OB/commands/admin_level/lift.py
+++ OBChat/OB/commands/admin_level/lift.py
@@ -1,88 +1,97 @@
 """
-lift function container module
+LiftCommand class container module
 """
 
+from OB.commands.base import BaseCommand
 from OB.constants import Privilege
 from OB.models import Ban, OBUser
 from OB.utilities.command import async_get_privilege
-from OB.utilities.database import async_delete, async_try_get
+from OB.utilities.database import async_delete, async_save, async_try_get
 from OB.utilities.event import send_system_room_message
 
-async def lift(args, sender, room):
+class LiftCommand(BaseCommand):
     """
     Allow one or more OBConsumers from a group a Room is associated with to rejoin a Room after
     being banned.
     TODO: Viewing a user from inside the Room will show they have been banned before.
-
-    Arguments:
-        args (list[string]): The usernames of OBUsers whose bans to lift. Should have length 1 or
-            more.
-        sender (OBUser): The OBUser who issued the command.
-        room (Room): The Room the command was sent from.
     """
 
-    # Remove duplicates
-    args = list(dict.fromkeys(args))
+    async def check_initial_errors(self):
+        """
+        See BaseCommand.check_initial_errors().
+        """
 
-    error_message = ""
+        # Is an anonymous/unauthenticted user
+        if self.sender_privilege < Privilege.AuthUser:
+            self.sender_receipt = (
+                "You are far from one who can lift bans. Log in and prove yourself an Admin."
+            )
+        # Is authenticated, but not an admin
+        elif self.sender_privilege < Privilege.Admin:
+            self.sender_receipt = (
+                "A mere mortal like yourself does not have the power to lift bans. Try to /apply "
+                "to be an Admin and perhaps you may obtain this power if you are worthy."
+            )
+        # Missing target arguments
+        elif not self.args:
+            self.sender_receipt = "Usage: /lift <user1> <user2> ..."
 
-    # Check for initial errors
-    if not sender.is_authenticated or sender.is_anon:
-        error_message = (
-            "You are far from one who can lift bans. Log in and prove yourself an Admin."
+        return not self.sender_receipt
+
+    async def check_arguments(self):
+        """
+        See BaseCommand.check_initial_errors().
+        """
+
+        for username in self.args:
+            arg_user = await async_try_get(OBUser, username=username)
+
+            if arg_user:
+                arg_ban = await async_try_get(Ban, user=arg_user, room=self.room)
+                issuer = await async_try_get(OBUser, ban_issued=arg_ban)
+                issuer_privilege = await async_get_privilege(issuer, self.room)
+                sender_privilege = await async_get_privilege(self.sender, self.room)
+            else:
+                arg_ban = None
+
+            # Target user is not present or does not have an active ban
+            if not arg_user or not arg_ban:
+                error_messages += [
+                    f"No user named {username} has been banned from this room. How can "
+                    "one lift that which has not been banned?"
+                ]
+            # Target user was banned by someone with higher privilege
+            elif issuer_privilege >= sender_privilege and issuer != self.sender:
+                error_messages += [
+                    f"{username} was banned by {issuer}. You cannot lift a ban issued by a "
+                    "user of equal or higher privilege than yourself. If you REALLY want to lift "
+                    "this ban you can /elevate to a higher authority."
+                ]
+            else:
+                self.valid_targets += [(arg_ban, arg_user)]
+
+        return bool(self.valid_targets)
+
+    async def execute_implementation(self):
+        """
+        Lifts the ban.
+        Note that a lifted ban is not deleted, but it is marked as lifted (see OB.models.ban).
+        Constructs strings to send back to the sender.
+        The sender receipt includes per-argument error messages.
+        """
+
+        lift_message_body = []
+
+        for lifted_ban, lifted_user in self.valid_targets:
+            # Mark the ban as lifted
+            lifted_ban.is_lifted = True
+            await async_save(lifted_ban)
+
+            lift_message_body += [f"   {lifted_user}"]
+
+        self.sender_receipt += (
+            # Add an extra newline to separate argument error messages from lift receipt
+            [("\n" if self.sender_receipt else "") + "Ban lifted:"] +
+            lift_message_body +
+            ["Fully reformed and ready to integrate into society."]
         )
-    elif await async_get_privilege(sender, room) < Privilege.Admin:
-        error_message = (
-            "A mere mortal like yourself does not have the power to lift bans. Try to /apply to be"
-            " an Admin and perhaps you may obtain this power if you are worthy."
-        )
-    elif not args:
-        error_message = "Usage: /lift <user1> <user2> ..."
-
-    # Send error message back to the issuing user
-    if error_message:
-        await send_system_room_message(error_message, room, [sender])
-        return
-
-    valid_lifts = []
-    error_messages = []
-
-    # Check for per-argument errors
-    for username in args:
-        arg_user = await async_try_get(OBUser, username=username)
-
-        if arg_user:
-            arg_ban = await async_try_get(Ban, user=arg_user, room=room)
-            issuer = await async_try_get(OBUser, ban_issued=arg_ban)
-            issuer_privilege = await async_get_privilege(issuer, room)
-            sender_privilege = await async_get_privilege(sender, room)
-        else:
-            arg_ban = None
-
-        if not arg_user or not arg_ban:
-            error_messages += [
-                f"No user named {username} has been banned from this room. How can "
-                "one lift that which has not been banned?"
-            ]
-        elif issuer_privilege >= sender_privilege and issuer != sender:
-            error_messages += [
-                f"{username} was banned by {issuer}. You cannot lift a ban issued by a "
-                "user of equal or higher privilege than yourself. If you REALLY want to lift this "
-                "ban you can /elevate to a higher authority."
-            ]
-        else:
-            valid_lifts += [(arg_ban, arg_user)]
-
-    send_to_sender = error_messages + [("\n" if error_messages else "") + "Ban lifted:"]
-
-    for lifted_ban, lifted_user in valid_lifts:
-        # Delete the ban from the database
-        await async_delete(lifted_ban)
-
-        send_to_sender += [f"   {lifted_user}"]
-
-    if valid_lifts:
-        send_to_sender += ["Fully reformed and ready to integrate into society."]
-        await send_system_room_message("\n".join(send_to_sender), room, [sender])
-    elif error_messages:
-        await send_system_room_message("\n".join(error_messages), room, [sender])
