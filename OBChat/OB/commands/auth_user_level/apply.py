faff4412d5e66500a1a13ad38c1692117f79fa7f
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-08-04 19:18:20 -0700

    Modified OB.commands.auth_user_level.apply to use inherit from BaseCommand class

--- OBChat/OB/commands/auth_user_level/apply.py
+++ OBChat/OB/commands/auth_user_level/apply.py
@@ -1,67 +1,90 @@
 """
-apply function container module
+ApplyCommand class container module.
 """
 
+from OB.commands.base import BaseCommand
 from OB.constants import Privilege
 from OB.models import Admin, OBUser
 from OB.utilities.command import async_get_privilege
 from OB.utilities.database import async_filter, async_get
 from OB.utilities.event import send_system_room_message
 
-async def apply(args, sender, room):
+class ApplyCommand(BaseCommand):
     """
     Sends a request for a user to be hired as an Admin or promoted to Unlimited Admin.
     The request is visible by all users with hiring privileges of the room.
     Optionally includes a message.
-
-    Arguments:
-        args (list[string]): An optional message to the users with hiring privileges.
-        sender (OBUser): The OBUser who issued the command.
-        room (Room): The Room the command was sent from.
     """
 
-    error_message = ""
-    sender_privilege = await async_get_privilege(sender, room)
+    def __init__(self):
+        """
+        Apply requires an instance variable for sending the message to specific users.
+        """
 
-    # Check for errors
-    if not sender.is_authenticated or sender.is_anon:
-        error_message = (
-            "You can't get hired looking like that! Clean yourself up and make an account first."
+        super().__init__()
+        self.targets_message = []
+
+    async def check_initial_errors(self):
+        """
+        See BaseCommand.check_initial_errors().
+        """
+
+        # Is an anonymous/unauthenticated user
+        if self.sender_privilege < Privilege.AuthUser:
+            self.sender_receipt += [
+                "You can't get hired looking like that! Clean yourself up and make an account "
+                "first."
+            ]
+        # Is already an Unlimited Admin
+        elif self.sender_privilege >= Privilege.UnlimitedAdmin:
+            self.sender_receipt += [
+                "You're already a big shot Unlimited Admin! There's nothing left to apply to."
+            ]
+
+        return not self.sender_receipt
+
+    async def execute(self):
+        """
+        Gathers recipients for the application.
+        Constructs strings to send back to the sender and to those who may hire them.
+        Arguments can only be a message to send along with the application, so they do not need to
+        be checked for errors like with other commands.
+        """
+
+        # Gather recipients
+        if self.sender_privilege < Privilege.Admin:
+            unlimited_admins = await async_filter(
+                Admin,
+                room=self.room,
+                is_limited=False,
+                is_revoked=False
+            )
+
+            for adminship in unlimited_admins:
+                self.valid_targets += [await async_get(OBUser, adminship=adminship)]
+
+        self.valid_targets = [self.room.owner]
+
+        # Construct strings
+        user_suffix = " [Admin]" if self.sender_privilege == Privilege.Admin else ""
+        position_prefix = "Unlimited " if self.sender_privilege == Privilege.Admin else ""
+        application_message = " ".join(self.args) if self.args else None
+
+        application_body = [
+            f"   User: {self.sender}{user_suffix}",
+            f"   Position: {position_prefix}Admin",
+            f"   Message: {application_message}"
+        ]
+
+        self.sender_receipt += (
+            # Add an exra newline to separate argument error messages from ban receipt
+            [("\n" if self.sender_receipt else "") + "Application sent:"] +
+            application_body +
+            ["Hopefully the response doesn't start with: \"After careful consideration...\""]
         )
-    elif sender_privilege >= Privilege.UnlimitedAdmin:
-        error_message = "You're already a big shot! There's nothing left to apply to."
 
-    # Send error message back to issuing user
-    if error_message:
-        await send_system_room_message(error_message, room, [sender])
-        return
-
-    # Construct the application and receipt
-    user_suffix = " [Admin]" if sender_privilege == Privilege.Admin else ""
-    position_prefix = "Unlimited " if sender_privilege == Privilege.Admin else ""
-    application_message = " ".join(args) if args else None
-
-    application = "\n".join([
-        f"Application Received",
-        f"   User: {sender}{user_suffix}",
-        f"   Position: {position_prefix}Admin",
-        f"   Message: {application_message}",
-        f"To hire this user, use /hire."
-    ])
-
-    receipt = (
-        "Your application was received. Hopefully the response doesn't start with: \"After careful"
-        " consideration...\""
-    )
-
-    # Gather recipients
-    if sender_privilege == Privilege.Admin:
-        recipients = [room.owner]
-    else:
-        recipients = []
-        for adminship in await async_filter(Admin, room=room, is_limited=False):
-            recipients += [await async_get(OBUser, adminship=adminship)]
-
-    # Send the application and receipt
-    await send_system_room_message(application, room, recipients)
-    await send_system_room_message(receipt, room, [sender])
+        self.targets_notification += (
+            "Application Received" +
+            application_body +
+            "To hire this user, use /hire."
+        )
