e53807a43e66fcc21068ac7e952616bcec835de7
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-08-10 18:56:49 -0700

    Added CreateCommand.check_arguments()

--- OBChat/OB/commands/auth_user_level/create.py
+++ OBChat/OB/commands/auth_user_level/create.py
@@ -3,7 +3,7 @@ CreateCommand class container module
 """
 
 from OB.commands.base import BaseCommand
-from OB.constants import GroupTypes
+from OB.constants import GroupTypes, Privilege
 from OB.models import Room
 from OB.utilities.database import async_save, async_try_get
 from OB.utilities.event import send_system_room_message
@@ -22,41 +22,32 @@ class CreateCommand(BaseCommand):
         if self.sender_privilege < Privilege.Admin:
             self.sender_receipt = ["Identify yourself! Must log in to create a room."]
         # Missing room name argument
-        elif not args:
+        elif not self.args:
             self.sender_receipt = ["Usage: /create <name>"]
         # Too many arguments
-        elif len(args) > 1:
+        elif len(self.args) > 1:
             self.sender_receipt = ["Room name cannot contain spaces."]
-        # Room with argument name already exists
-        elif existing_room:
-            self.sender_receipt = [f"Someone beat you to it. {existing_room} already exists."]
 
         return not self.sender_receipt
 
+    async def check_arguments(self):
+        """
+        See BaseCommand.check_initial_errors()
+        """
 
-    # Check for errors
-    if not args:
-        error_message = "Usage: /create <name>"
-    else:
         existing_room = await async_try_get(
             Room,
             group_type=GroupTypes.Room,
-            name=args[0].lower()
+            name=self.args[0].lower()
         )
 
-        if not sender.is_authenticated or sender.is_anon:
-            error_message = "Identify yourself! Must log in to create a room."
-        elif len(args) > 1:
-            error_message = "Room name cannot contain spaces."
-        elif existing_room:
-            error_message = f"Someone beat you to it. {existing_room} already exists."
+        # Room with argument name already exists
+        if existing_room:
+            self.sender_receipt += [f"Someone beat you to it. {existing_room} already exists."]
+            return False
 
-    # Send error message back to issuing user
-    if error_message:
-        await send_system_room_message(error_message, room, [sender])
-        return
+        return True
 
-    display_name = args[0] if not args[0].islower() else None
 
     # Save the new room
     created_room = await async_save(
