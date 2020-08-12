dea7830b333dd2f89a33c0f21eccd5e96b64e1b9
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-08-11 19:17:00 -0700

    Added DeleteCommand.execute_implementation()

--- OBChat/OB/commands/owner_level/delete.py
+++ OBChat/OB/commands/owner_level/delete.py
@@ -40,3 +40,30 @@ class DeleteCommand(BaseCommand):
         ):
             self.sender_receipt = ["Usage: /delete <room name> <owner username>"]
 
+    async def execute_implementation(self):
+        """
+        Kicks users, deletes Admins, deletes Bans, deletes Messages, and finally deletes the Room.
+        """
+
+        # Kick all users
+        for user in await async_model_list(self.room.occupants):
+            kick_event = {
+                "type": "kick",
+                "target_id": user.id
+            }
+            await send_room_event(self.room.id, kick_event)
+
+        # Delete all Admins
+        for admin in await async_filter(Admin, room=self.room):
+            await async_delete(admin)
+
+        # Delete all Bans
+        for ban in await async_filter(Ban, room=self.room):
+            await async_delete(ban)
+
+        # Delete all Messages
+        for message in await async_filter(Message, room=self.room):
+            await async_delete(message)
+
+        # Delete the room
+        await async_delete(self.room)
