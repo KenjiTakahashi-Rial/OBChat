ed66261b10bd0126de9ea2682e2fedc6cadc63b0
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-08-11 19:05:18 -0700

    Modified OB.commands.auth_user_level.delete to inherit from BaseCommand class

--- OBChat/OB/commands/owner_level/delete.py
+++ OBChat/OB/commands/owner_level/delete.py
@@ -2,58 +2,27 @@
 DeleteCommand class container module.
 """
 
+from OB.commands.base import BaseCommand
 from OB.constants import Privilege
 from OB.models import Admin, Ban, Message
 from OB.utilities.command import async_get_privilege
 from OB.utilities.database import async_delete, async_model_list, async_filter
 from OB.utilities.event import send_room_event, send_system_room_message
 
-async def delete(args, sender, room):
+class DeleteCommand(BaseCommand):
     """
     Deletes an existing Room database object.
     All Admin, Ban, and Message objects will be deleted as well.
-
-    Arguments:
-        args (list[string]): The names of Rooms to delete. Should have length 1 or more.
-        sender (OBUser): The OBUser who issued the command.
-        room (Room): The Room the command was sent from.
     """
 
-    error_message = ""
-    sender_privilege = await async_get_privilege(sender, room)
+    async def check_initial_errors(self):
+        """
+        See BaseCommand.check_initial_errors().
+        """
 
-    # Check for initial errors
-    if sender_privilege < Privilege.Owner:
-        error_message = (
-            "Trying to delete someone else's room? How rude. Only the room owner may delete a room"
-        )
-    elif not args or len(args) > 2 or args[0] != room.name or args[1] != sender.username:
-        error_message = "Usage: /delete <room name> <owner username>"
-
-    # Send error message back to the issuing user
-    if error_message:
-        await send_system_room_message(error_message, room, [sender])
-        return
-
-    # Kick all users
-    for user in await async_model_list(room.occupants):
-        kick_event = {
-            "type": "kick",
-            "target_id": user.id
-        }
-        await send_room_event(room.id, kick_event)
-
-    # Delete all Admins
-    for admin in await async_filter(Admin, room=room):
-        await async_delete(admin)
-
-    # Delete all Bans
-    for ban in await async_filter(Ban, room=room):
-        await async_delete(ban)
-
-    # Delete all Messages
-    for message in await async_filter(Message, room=room):
-        await async_delete(message)
-
-    # Delete the room
-    await async_delete(room)
+        # Is not the owner
+        if self.sender_privilege < Privilege.Owner:
+            error_message = (
+                "Trying to delete someone else's room? How rude. Only the room owner may delete a "
+                "room"
+            )
