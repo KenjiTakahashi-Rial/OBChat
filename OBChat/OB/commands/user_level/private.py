aa432e8b230c59d49c13f029c207b304a248b3ba
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-08-14 23:19:29 -0700

    Modified Modified OB.commands.auth_user_level.private to inherit from BaseCommand class

--- OBChat/OB/commands/user_level/private.py
+++ OBChat/OB/commands/user_level/private.py
@@ -1,33 +1,32 @@
 """
-private function container module.
+PrivateCommand class container module.
 """
 
+from OB.commands.base import BaseCommand
 from OB.models import OBUser
 from OB.utilities.database import async_try_get
 from OB.utilities.event import send_private_message, send_system_room_message
 
-async def private(args, sender, room):
+class PrivateCommand(BaseCommand):
     """
     Sends a private message from the user parameter to another OBUser.
     The private message will have its own OBConsumer group where only the two users will receive
     messages.
-
-    Arguments:
-        args (list[string]): The first item should be a username prepended with '/' and the
-            following strings should be words in the private message.
-        sender (OBUser): Not used, but included as a parameter so the function can be called from
-            the COMMANDS dict.
-        room (Room): The Room the command was sent from.
     """
 
-    error_message = ""
+    async def check_initial_errors(self):
+        """
+        See BaseCommand.check_initial_errors().
+        """
 
-    # Check for initial errors
-    if not args:
-        error_message = "Usage: /private /<user> <message>"
-    elif args[0][0] != '/':
-        error_message = "Looks like you forgot a \"/\" before the username. I'll let it slide."
-    else:
+        # Missing target and message arguments
+        if not self.args:
+            self.sender_receipt = ["Usage: /private /<user> <message>"]
+        # Invalid syntax
+        elif args[0][0] != '/':
+            self.sender_receipt = ["Looks like you forgot a \"/\" before the username. I'll let it slide."]
+
+
         # Check for per-argument errors
         recipient = await async_try_get(OBUser, username=args[0][1:])
         if not recipient:
@@ -38,11 +37,11 @@ async def private(args, sender, room):
         elif len(args) == 1:
             error_message = "No message specified. Did you give up at just the username?"
 
-    # Send error message back to issuing user
-    if error_message:
-        await send_system_room_message(error_message, room, [sender])
-        return
+        # Send error message back to issuing user
+        if error_message:
+            await send_system_room_message(error_message, room, [sender])
+            return
 
-    # Reconstruct message from args
-    message_text = " ".join(args[1:])
-    await send_private_message(message_text, sender, recipient)
+        # Reconstruct message from args
+        message_text = " ".join(args[1:])
+        await send_private_message(message_text, sender, recipient)
