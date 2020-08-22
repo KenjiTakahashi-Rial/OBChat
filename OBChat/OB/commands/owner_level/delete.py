8d253fb86e577b4d0def53948019555d5cb5f756
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-08-22 14:31:12 -0700

    Removed unused imports from DeleteCommand

--- OBChat/OB/commands/owner_level/delete.py
+++ OBChat/OB/commands/owner_level/delete.py
@@ -5,9 +5,8 @@ DeleteCommand class container module.
 from OB.commands.base import BaseCommand
 from OB.constants import Privilege
 from OB.models import Admin, Ban, Message
-from OB.utilities.command import async_get_privilege
 from OB.utilities.database import async_delete, async_model_list, async_filter, async_get_owner
-from OB.utilities.event import send_room_event, send_system_room_message
+from OB.utilities.event import send_room_event
 
 class DeleteCommand(BaseCommand):
     """
