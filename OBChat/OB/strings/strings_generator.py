7e173c9fc5f13bb3fab087d644c7bb97bc78e458
Kenji Takahashi-Rial <kenji.ob.taka@gmail.com>
2020-08-25 18:47:18 -0700

    Added duplicate checking to strings_generator

--- OBChat/OB/strings/strings_generator.py
+++ OBChat/OB/strings/strings_generator.py
@@ -32,11 +32,23 @@ csv_reader = reader(strings_spreadsheet)
 # The first row is just column headers
 next(csv_reader)
 
+string_ids = set()
+strings = set()
 i = 0
 
 for row in csv_reader:
     # The ID is the 0th element, the English string is the 1st
-    string_id_module.write(f"    {row[0]} = \"{row[1]}\"\n")
+    string_id = row[0]
+    string = row[1]
+
+    if string_id in string_ids:
+        print(f"WARNING: Duplicate StringId at row {i + 2}. {string_id}")
+    if string in strings:
+        print(f"WARNING: Duplicate string at row {i + 2}. {string}")
+
+    string_id_module.write(f"    {string_id} = \"{string}\"\n")
+    string_ids.add(string_id)
+    strings.add(string)
     i += 1
 
 strings_spreadsheet.close()
